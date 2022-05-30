f3bb3f829dbd2625de7dc56403eafd9a21cc0922
LUU DUONG <luudinhduong2806@gmail.com>
2022-05-30 07:36:20 +0700

    laravel basic 2

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,7 +14,6 @@ class UsersController extends Controller
     public function index()
     {
         $data = Users::users();
-
         return view("users.users", compact('data'));
     }
 
@@ -26,28 +25,24 @@ class UsersController extends Controller
     public function store(CreateUsersRequest $request)
     {
         $data = Users::store($request);
-
         return redirect()->route('users.index');
     }
 
     public function show($id)
     {
         $record = Users::show($id);
-
         return view('users.edit', compact('record'));
     }
 
     public function edit(Request $request, $id)
     {
         $record = Users::edit($id, $request);
-
         return redirect()->route('users.index');
     }
 
     public function destroy(Request $request, $id)
     {
         $user = Users::remove($request, $id);
-
         return redirect()->route('users.index');
     }
 }
